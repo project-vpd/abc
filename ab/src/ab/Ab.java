76d1ac6f2302aa2f0a7e91a46d92a292847e8653
project-vpd <59731706+project-vpd@users.noreply.github.com>
2020-01-10 19:33:36 +0530

    Update Ab.java
    hi removed

--- ab/src/ab/Ab.java
+++ ab/src/ab/Ab.java
@@ -5,7 +5,6 @@ public class Ab {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Hello");
-        System.out.println("Hi");
     }
     
 }
